a84c0d9b532372b52b5dae3b960eeb1792d58667
msteini82 <45187374+msteini82@users.noreply.github.com>
2022-02-27 22:00:09 +0100

    Update SharedPreferencesCookieStore.kt
    Implement HttpCookieArrayAdapter for gson

--- cookie-store/src/main/java/net/gotev/cookiestore/SharedPreferencesCookieStore.kt
+++ cookie-store/src/main/java/net/gotev/cookiestore/SharedPreferencesCookieStore.kt
@@ -2,10 +2,88 @@ package net.gotev.cookiestore
 
 import android.content.Context
 import android.util.Log
-import com.google.gson.Gson
+import com.google.gson.*
 import com.google.gson.reflect.TypeToken
+import java.lang.reflect.Type
 import java.net.HttpCookie
 import java.net.URI
+
+class HttpCookieArrayAdapter : JsonSerializer<ArrayList<HttpCookie>>, JsonDeserializer<ArrayList<HttpCookie>> {
+
+    override fun serialize(
+        src: ArrayList<HttpCookie>?,
+        typeOfSrc: Type?,
+        context: JsonSerializationContext?
+    ): JsonElement {
+
+        val whenCreatedField = HttpCookie::class.java.getDeclaredField("whenCreated")
+        whenCreatedField.isAccessible = true
+
+        val array = JsonArray()
+
+        if (src != null) {
+            for (cookie in src) {
+
+                val obj = JsonObject()
+
+                obj.addProperty("name", cookie.name)
+                obj.addProperty("value", cookie.value)
+                obj.addProperty("domain", cookie.domain)
+                obj.addProperty("path", cookie.path)
+                obj.addProperty("httpOnly", cookie.isHttpOnly)
+                obj.addProperty("secure", cookie.secure)
+                obj.addProperty("toDiscard", cookie.discard)
+                obj.addProperty("maxAge", cookie.maxAge)
+                obj.addProperty("whenCreated", whenCreatedField.get(cookie) as Long)
+                obj.addProperty("version", cookie.version)
+
+                array.add(obj)
+
+            }
+        }
+
+        return array
+
+    }
+
+    override fun deserialize(
+        json: JsonElement?,
+        typeOfT: Type?,
+        context: JsonDeserializationContext?
+    ): ArrayList<HttpCookie> {
+
+        val whenCreatedField = HttpCookie::class.java.getDeclaredField("whenCreated")
+        whenCreatedField.isAccessible = true
+
+        val list = arrayListOf<HttpCookie>()
+
+        if (json != null) {
+            for (el in json as JsonArray) {
+
+                val obj = el.asJsonObject
+
+                val cookie = HttpCookie(obj["name"].asString, obj["value"].asString)
+
+                cookie.domain = obj["domain"].asString
+                cookie.path = obj["path"].asString
+                cookie.isHttpOnly = obj["httpOnly"].asBoolean
+                cookie.secure = obj["secure"].asBoolean
+                cookie.discard = obj["toDiscard"].asBoolean
+                cookie.maxAge = obj["maxAge"].asLong
+                cookie.version = obj["version"].asInt
+
+                whenCreatedField.set(cookie, obj["whenCreated"].asLong)
+
+                list.add(cookie)
+
+            }
+        }
+
+        return list
+
+    }
+
+}
 
 open class SharedPreferencesCookieStore(
     context: Context,
@@ -13,7 +91,7 @@ open class SharedPreferencesCookieStore(
 ) : InMemoryCookieStore(name) {
 
     private val preferences = context.getSharedPreferences(name, Context.MODE_PRIVATE)
-    private val gson = Gson()
+    private val gson = GsonBuilder().registerTypeAdapter(listType, CookieArrayAdapter()).create()
     private val listType = object : TypeToken<ArrayList<HttpCookie>>() {}.type
 
     init {
